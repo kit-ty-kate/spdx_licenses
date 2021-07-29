8eebee19d3416863cee571b99daac7f204e11966
Kate <kit.ty.kate@disroot.org>
2021-07-29 10:18:47 +0100

    Add a more complete test for precedence rule

--- tests/tests.ml
+++ tests/tests.ml
@@ -28,9 +28,26 @@ let () =
       test "parse fails on invalid exceptions"
         (parse "MIT WITH TEST")
         "Error (`InvalidExceptionID TEST)";
-      test "parse has the right precedence rule"
+      test "parse has the right precedence rule (short)"
         (parse "LGPL-2.1-only OR BSD-3-Clause AND MIT")
         "Ok (LGPL-2.1-only OR (BSD-3-Clause AND MIT))";
+      test "parse has the right precedence rule (long)"
+        (parse "LGPL-2.1-only WITH OCaml-LGPL-linking-exception
+                OR BSD-3-Clause AND MIT AND ISC AND BitTorrent-1.1 OR
+                BSL-1.0 OR CC-BY-1.0 OR CC-BY-2.5 AND
+                MPL-2.0 AND 0BSD WITH OCaml-LGPL-Linking-exception")
+        "Ok (LGPL-2.1-only WITH OCaml-LGPL-linking-exception OR \
+             (BSD-3-Clause AND MIT AND ISC AND BitTorrent-1.1) OR \
+             BSL-1.0 OR CC-BY-1.0 OR \
+             (CC-BY-2.5 AND MPL-2.0 AND 0BSD WITH OCaml-LGPL-linking-exception))";
+      test "parse has the right precedence rule (long, reversed)"
+        (parse "LGPL-2.1-only WITH OCaml-LGPL-linking-exception
+                AND BSD-3-Clause OR MIT OR ISC OR BitTorrent-1.1 AND
+                BSL-1.0 AND CC-BY-1.0 AND CC-BY-2.5 OR
+                MPL-2.0 OR 0BSD WITH OCaml-LGPL-Linking-exception")
+        "Ok ((LGPL-2.1-only WITH OCaml-LGPL-linking-exception AND BSD-3-Clause) OR \
+             MIT OR ISC OR (BitTorrent-1.1 AND BSL-1.0 AND CC-BY-1.0 AND CC-BY-2.5) OR \
+             MPL-2.0 OR 0BSD WITH OCaml-LGPL-linking-exception)";
       test "parse has the right case sensitivity"
         (parse "BsD-2-clAUsE")
         "Ok (BSD-2-Clause)";
