857ba92a834d3ec45b041ec8ac082874f8d0f973
Kate <kit.ty.kate@disroot.org>
2021-07-29 11:31:30 +0100

    Document the API

--- src/spdx_licenses.mli
+++ src/spdx_licenses.mli
@@ -1,10 +1,14 @@
 (* SPDX-License-Identifier: MIT *)
 
+(** simple-expression *)
 type simple_license =
-  | LicenseID of string
-  | LicenseIDPlus of string
-  | LicenseRef of (string * string)
+  | LicenseID of string (** license-id *)
+  | LicenseIDPlus of string (** license-id '+' (the '+' isn't contained in the string) *)
+  | LicenseRef of (string * string) (** A SPDX user defined license reference.
+                                        The first string is the document reference and can be empty.
+                                        The second string is the license reference and is NOT empty. *)
 
+(** license-expression *)
 type t =
   | Simple of simple_license
   | WITH of simple_license * string
@@ -18,9 +22,19 @@ type errors = [
 ]
 
 val parse : string -> (t, [> errors]) Result.t
+(** [parse str] parses [str] according to the syntax described in:
+    https://spdx.github.io/spdx-spec/appendix-IV-SPDX-license-expressions/ *)
 
 val to_string : t -> string
+(** [to_string license] returns a normalized string corresponding to [license]
+    in a valid SPDX license expression format. *)
 
 val valid_license_ids : string list
+(** [valid_license_ids] gives the list of valid license IDs.
+    The list does not contain deprecated licenses.
+    See Appendix I.1: https://spdx.github.io/spdx-spec/appendix-I-SPDX-license-list/ *)
 
 val valid_exception_ids : string list
+(** [valid_exception_ids] gives the list of valid exception IDs.
+    The list does not contain deprecated exceptions.
+    See Appendix I.2: https://spdx.github.io/spdx-spec/appendix-I-SPDX-license-list/ *)
